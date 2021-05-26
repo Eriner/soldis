405d28089aab8fa654d19cc953db69ea13eb2bd5
Matt Hamilton <m@tthamilton.com>
2021-05-26 11:59:20 -0400

    broke, but found the libs
    Signed-off-by: Matt Hamilton <m@tthamilton.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/eriner/soldis
 
 go 1.16
 
-require github.com/ethereum/go-ethereum v1.10.3 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/ethereum/go-ethereum v1.10.3
+)
--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/ethereum/go-ethereum/core/asm"
+)
+
 func main() {
+	dat, err := ioutil.ReadFile("testdata/GameStop.bin")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	out, err := asm.Disassemble(dat)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	spew.Dump(out)
+	// log.Fatalln(asm.PrintDisassembled(out[0]))
 }
