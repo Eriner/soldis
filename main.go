a724220a0068b86318b17fa637a5462f89577c98
Matt Hamilton <m@tthamilton.com>
2021-05-26 12:26:59 -0400

    more notes
    Signed-off-by: Matt Hamilton <m@tthamilton.com>

--- main.go
+++ main.go
@@ -13,12 +13,14 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	out, err := asm.Disassemble(contractBin)
+	out, err := asm.Disassemble(contractBin[148:])
 	if err != nil {
 		// Currently, the Disassemble function from go-ethereum chokes on
 		// some opcodes in the GME contract bytecode. Not sure if this should
 		// be expected or not, as the etherscan disassembler also chokes on some
 		// opcodes. /shrug
+		// Maybe it is an offset problem? Maybe it doesn't like disassembling the
+		// contract initializer?
 		// Error:
 		// 2021/05/26 12:12:49 incomplete push instruction at 11013
 		log.Fatalln(err)
